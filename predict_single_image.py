622c646f9e874848d7e3bff957bb36a8b9a40fd7
Darwin757 <darwin.rocker@gmail.com>
2024-03-29 15:10:04 +0100

    Fixed prediction shape mismatch and optimized single image prediction process.

--- predict_single_image.py
+++ predict_single_image.py
@@ -1,24 +1,30 @@
 import numpy as np
-from tensorflow.keras.models import load_model
+import tensorflow as tf
 from utils import load_and_preprocess_image
 import matplotlib.pyplot as plt
+from custom_data_loader import CustomDataLoader
+
+# Initialize the CustomDataLoader with your dataset path
+dataset_path = "ThoraxScanData"
+data_loader = CustomDataLoader(dataset_path=dataset_path)
 
 # Path to the saved model and new image
 model_path = 'best_model.h5'
 image_path = 'ThoraxScanData/test/Viral Pneumonia/58.jpg'
 
 # Load the trained model
-model = load_model(model_path)
+model = tf.keras.models.load_model(model_path)
 
 # Preprocess the image
 preprocessed_image = load_and_preprocess_image(image_path)
+
 
 # Predict the class of the image
 prediction = model.predict(preprocessed_image)
 predicted_class_index = np.argmax(prediction, axis=1)[0]
 
 # Assuming you have a list of class names
-class_names = ['Lung_Opacity', 'Normal', 'Viral Pneumonia']
+class_names = data_loader.class_names
 predicted_class_name = class_names[predicted_class_index]
 
 print(f"Predicted class: {predicted_class_name}")
